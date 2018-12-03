6d5872be77757164d260987c7e3f4cb78cec79d6
Vash <diegovm14@gmail.com>
2018-12-03 23:58:51 +0100

    Update Horses.java

--- Horses.java
+++ Horses.java
@@ -1,2 +1,5 @@
 /luego lo hago ahora estoy ocupado
 25 caballos, encuentra los 3 mas rapidos, cuantas carreras si pistas para 5
+  5-5-5-5-5
+  5 mejores, 5 segundos, 5 terceros
+  4+1 4+1 en total 10 carreras
